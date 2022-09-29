b38c233c936f07a8a0d6c27dba7f17d04118d4a6
Nicklas Utgaard <nutgaard@gmail.com>
2022-09-29 14:34:16 +0200

    App that mutates at startup, and kills itself after a random amount of time

--- src/randomapp.js
+++ src/randomapp.js
@@ -1,24 +1,24 @@
 var fs = require('fs');
 var STORAGE = "/usr/share/nginx/html/deploytest2"
 
+var rndFileName = process.env.RND_FILE_NAME;
+var jsContent = "console.log('"+rndFileName+"');";
+var indexHtml = fs.readFileSync(STORAGE + "/index.html")
+    .toString()
+    .replace(/{{rndFileName}}/g, rndFileName);
 
 async function pickapp(req) {
     const url = req.variables.request_uri;
     const parts = url.split('/');
     let file = parts[parts.length - 1];
-    if (!file.includes('.')) {
-        file = "index.html";
-    }
 
-    const appStorage = Math.random() > 0.5 ? "/src1" : "/src2";
-    let data = "innlasting";
-    try {
-        data = fs.readFileSync(STORAGE + appStorage + "/" + file);
-    } catch (err) {
-        req.return(404);
-        return;
+    if (!file.includes('.') || file.includes("index.html")) {
+        req.return(200, indexHtml);
+    } else if (file.includes(rndFileName)) {
+        req.return(200, jsContent);
+    } else {
+        req.return(404, rndFileName);
     }
-    req.return(200, data);
 }
 
 export default {
